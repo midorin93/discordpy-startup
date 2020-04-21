e93a9c477f45f59b37592bd4e9ca8f9b2b546fec
midorin93 <63625554+midorin93@users.noreply.github.com>
2020-04-22 02:27:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,4 @@
+
 from discord.ext import commands
 import os
 import traceback
@@ -16,7 +17,6 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
- 
 
 
 bot.run(token)
