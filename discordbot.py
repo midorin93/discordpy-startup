190ec4b9db1bde4533ae1d6dbf88295e1d1d3e98
midorin93 <63625554+midorin93@users.noreply.github.com>
2020-04-22 02:25:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,6 @@
 from discord.ext import commands
 import os
 import traceback
-import random
-import asyncio #sleepを使うのに必要
-import discord ##discordでBOTを使うのにこれが必ずいる
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -19,19 +16,7 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
-@bot.command()
-if message.content == "スロット":
-kakuritsu = random.randint(1, 399)
-slot_list = [':yamasho:', ':NK_3rd:', ':NK_2nd:', ':mocchiup:', ':higasho:', ':domenfuuuuck:', ':ganjaR:']
-A = random.choice(slot_list)
-B = random.choice(slot_list)
-C = random.choice(slot_list)
-if int(kakuritsu) == int(1): #確率は1/399
-await client.send_message(message.channel, "イク！")
-await asyncio.sleep(2) #2秒間待ってやる
-await client.send_message(message.channel, ':abe:', ':abe:', ':abe:') #abeだけ出るように指定
-else:
-await client.send_message(message.channel, "%s%s%s" % (A, B, C))
+ 
+
 
 bot.run(token)
