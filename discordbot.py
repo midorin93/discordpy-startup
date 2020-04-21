17907a563d048937bb18f416bc54f6720bd35071
midorin93 <63625554+midorin93@users.noreply.github.com>
2020-04-22 03:31:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,13 +9,6 @@ bot = commands.Bot()
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, message, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-
-
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
@@ -29,13 +22,11 @@ async def on_message(message):
   B = random.choice(slot_list)
   C = random.choice(slot_list)
       if int(kakuritsu) == int(1): #確率は1/399
-      await bot.send_message(message.channel, "イク！")
-      await asyncio.sleep(2) #2秒間待ってやる
-      await bot.send_message(message.channel, ':abe:', ':abe:', ':abe:') #abeだけ出るように指定
-          else:
-          await bot.send_message(message.channel, "%s%s%s" % (A, B, C))    
-    
-    
-    
+          await bot.send_message(message.channel, "イク！")
+          await asyncio.sleep(2) #2秒間待ってやる
+          await bot.send_message(message.channel, ':abe:', ':abe:', ':abe:') #abeだけ出るように指定
+              else:
+              await bot.send_message(message.channel, "%s%s%s" % (A, B, C))    
+
    
 bot.run(token)
