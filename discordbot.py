13e33d62d82467408b5259b562b4f326d75cd843
midorin93 <63625554+midorin93@users.noreply.github.com>
2020-04-22 03:58:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,17 +16,17 @@ async def ping(ctx):
 @bot.event
 async def on_message(message):
   if message.content == "スロット":
-  kakuritsu = random.randint(1, 399)
   slot_list = [':yamasho:', ':NK_3rd:', ':NK_2nd:', ':mocchiup:', ':higasho:', ':domenfuuuuck:', ':ganjaR:']
   A = random.choice(slot_list)
   B = random.choice(slot_list)
   C = random.choice(slot_list)
-      if int(kakuritsu) == int(1): #確率は1/399
-          await bot.send_message(message.channel, "イク！")
-          await asyncio.sleep(2) #2秒間待ってやる
-          await bot.send_message(message.channel, ':abe:', ':abe:', ':abe:') #abeだけ出るように指定
-              else:
-              await bot.send_message(message.channel, "%s%s%s" % (A, B, C))    
+    await bot.send_message(message.channel, "%s%s%s" % (A, B, C))    
 
+    #kakuritsu = random.randint(1, 399)
+    #  if int(kakuritsu) == int(1): #確率は1/399
+    #      await bot.send_message(message.channel, "イク！")
+    #      await asyncio.sleep(2) #2秒間待ってやる
+    #      await bot.send_message(message.channel, ':abe:', ':abe:', ':abe:') #abeだけ出るように指定
+    #          else:
    
 bot.run(token)
